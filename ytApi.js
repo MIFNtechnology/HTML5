baf999a1b11856225a32005ad18a2483a53eda30
Kaitou E <102132258+ejunds@users.noreply.github.com>
2024-05-31 19:31:27 +0800

    update ytApi.js

---
+++ ytApi.js
@@ -0,0 +1,15 @@
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'CSnwqWLGWuo',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
