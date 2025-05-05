9eb54c6ef6c26784c04b096c27913365bcd3a8c1
Kaitou E <102132258+ejunds@users.noreply.github.com>
2025-05-06 03:17:13 +0800

    update script.js

--- content/TvAlhijrah_live_stream/script.js
+++ content/TvAlhijrah_live_stream/script.js
@@ -1,37 +1,44 @@
 async function init() {
-      const video = document.getElementById('video');
-      const ui = video['ui'];
-      const controls = ui.getControls();
-      const player = controls.getPlayer();
-
-      player.configure({
-        drm: {
-          clearKeys: {
-            '41a42fb5af457501c11bb9b158d5a110': '57a8d6f9e3832887db18ea6071118736'
+	const video = document.getElementById('video');
+	const ui = video['ui'];
+	const controls = ui.getControls();
+	const player = controls.getPlayer();
+	const config = {
+		enableKeyboardPlaybackControls: true,
+		'seekBarColors': {
+			base: 'rgba(255,255,255,.2)',
+			buffered: 'rgba(255,255,255,.4)',
+			played: 'rgb(255,0,0)',
+          },
+         };
+            ui.configure(config);
+            
+            player.configure({
+            	drm: {
+            	  clearKeys: {
+            	// 'key-id-in-hex': 'key-in-hex',
+            "4745ac4464797aa9d5ed653c1e987917": "ffe2dd898757fb14c156b3e014058ecc"
+            }
           }
+        });
+        player.getNetworkingEngine().registerRequestFilter(function(type, request) {
+        	if (type == shaka.net.NetworkingEngine.RequestType.SEGMENT) {
+        	//   request.headers['authorization'] = '';
         }
       });
-
-      player.setTextTrackVisibility(true); // Auto ON caption
-
-      window.player = player;
-      window.ui = ui;
-
-      player.addEventListener('error', onPlayerErrorEvent);
-      controls.addEventListener('error', onUIErrorEvent);
-
-      try {
-        await player.load('https://linearjitp-playback.astro.com.my/dash-wv/linear/1113/default_ott.mpd');
-        console.log('✅ Stream loaded!');
-      } catch (error) {
-        onPlayerError(error);
-      }
-    }
-
-    function onPlayerErrorEvent(errorEvent) { onPlayerError(event.detail); }
-    function onPlayerError(error) { console.error('Error code', error.code, 'object', error); }
-    function onUIErrorEvent(errorEvent) { onPlayerError(event.detail); }
-    function initFailed(errorEvent) { console.error('❌ UI load failed!'); }
-
+      
+      player.setTextTrackVisibility(true);
+    //player.configure({drm:{servers:{'com.widevine.alpha':'https://license.dstv.com/widevine/getLicense?CrmId=afl&AccountId=afl&ContentId=SH2&SessionId=D3C00F885C24B9C6&Ticket=C839A8D71AB94299'}}});
+    
+    window.player = player;
+    window.ui = ui;
+    player.addEventListener('error', onPlayerErrorEvent);
+    controls.addEventListener('error', onUIErrorEvent);
+    try{await player.load('https://unifi-live01.secureswiftcontent.com/UnifiHD/live07.mpd');
+    console.log('The video has now been loaded!');} catch (error) {onPlayerError(error);}}
+    function onPlayerErrorEvent(errorEvent) {onPlayerError(event.detail);}
+    function onPlayerError(error) {console.error('Error code', error.code, 'object', error);}
+    function onUIErrorEvent(errorEvent) {onPlayerError(event.detail);}
+    function initFailed(errorEvent) {console.error('Unable to load the UI library!');}
     document.addEventListener('shaka-ui-loaded', init);
     document.addEventListener('shaka-ui-load-failed', initFailed);
